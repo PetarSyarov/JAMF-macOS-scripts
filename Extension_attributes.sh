dcf037934dcd6cc1a0ca43c08d292307fe8457d4
Petar <petarsyarov@icloud.com>
2025-12-31 19:48:19 +0200

    Add files via upload

---
+++ Extension_attributes.sh
@@ -0,0 +1,28 @@
+# Get LocalHostName
+localhostName=$(scutil --get LocalHostName 2>/dev/null)
+
+# If localhostname is empty, set a fallback
+if [ -z "$localhostName" ]; then
+    localhostName="(No LocalHostName Set)"
+fi
+
+# Output in JAMF EA format
+echo "<result>$localhostName</result>"
+
+
+
+-----------------------------------------------------------------------------------------------------------------------
+
+
+
+# Get HostName
+hostName=$(scutil --get HostName 2>/dev/null)
+
+# If hostname is empty, set a fallback
+if [ -z "$hostName" ]; then
+    hostName="(No HostName Set)"
+fi
+
+# Output in JAMF EA format
+echo "<result>$hostName</result>"
+
